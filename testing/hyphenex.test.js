f8578906d44d975f14a1e51ef7f67edf47660d45
zaidajani <zaid.metal.ajani@gmail.com>
2021-11-25 16:38:09 +0530

    final touches done

---
+++ testing/hyphenex.test.js
@@ -0,0 +1,24 @@
+const hyphenex = require("../hyphenex");
+
+describe('Testing functions', () => {
+  test("hyphenate this string", () => {
+    expect(hyphenex.hyphenate("hyphenate this beautiful string")).toBe(
+      "hyphenate-this-beautiful-string"
+    );
+  });
+  test("dehyphenate this string", () => {
+    expect(hyphenex.deHyphenate("hyphenate-this-beautiful-string")).toBe(
+      "hyphenate this beautiful string"
+    );
+  });
+  test('Dynamically hyphenate this string', () => {
+    expect(hyphenex.hyphenateDynamically("This string will be hyphenated dynamically", "_")).toBe(
+      "This_string_will_be_hyphenated_dynamically"
+    );
+  });
+  test('Dynamically dehyphenate this string', () => {
+    expect(hyphenex.deHyphenateDynamically("This+string+will+be+dehyphenated+dynamically", "+")).toBe(
+      "This string will be dehyphenated dynamically"
+    )
+  });
+});
